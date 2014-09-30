2a8dee2f8e046db6d8548c6dc3ddb63307c55486
csywind008 <csyios@163.com>
2014-09-30 11:39:28 +0800

    add new  a Project

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RTSPAVdemo
+//
+//  Created by Peter James on 14-9-30.
+//  Copyright (c) 2014年 beiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RTSPAVdemo
+//
+//  Created by Peter James on 14-9-30.
+//  Copyright (c) 2014年 beiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
